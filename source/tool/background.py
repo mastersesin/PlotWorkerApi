6a157401816bbf549c5604a8b6c3fac87fdfeb85
Ty Tran <ty@autonomous.nyc>
2021-07-05 12:45:40 +0700

    Update background.py

--- source/tool/background.py
+++ source/tool/background.py
@@ -3,8 +3,46 @@ import threading
 
 from sqlalchemy import and_
 
-from source import session
-from source.object.sql import Credential
+from flask import Flask
+from flask_cors import CORS
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+engine = create_engine('postgresql:///trongty', echo=False)
+Base = declarative_base()
+from sqlalchemy import Boolean, Column, Integer, String, LargeBinary, ForeignKey, REAL, Numeric, DateTime, DECIMAL, \
+    BIGINT
+from source import Base
+import json
+
+
+class Credential(Base):
+    __tablename__ = 'credential'
+    id = Column(Integer, primary_key=True)
+    json_credential = Column(String, nullable=False)
+    email = Column(String, nullable=False)
+    last_used_timestamp = Column(Integer, default=0)
+    used_times = Column(Integer, default=0)
+
+    def __repr__(self):
+        return '<Id: {}>'.format(self.id)
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'json_credential': json.loads(self.json_credential),
+            'email': self.email,
+            'last_used_time': self.last_used_timestamp,
+            'used_times': self.used_times
+        }
+
+
+Base.metadata.create_all(engine)
+Session = sessionmaker()
+Session.configure(bind=engine)
+session = Session()
+session.commit()
 
 
 class BackgroundWorker(threading.Thread):
@@ -28,7 +66,7 @@ class BackgroundWorker(threading.Thread):
         while True:
             self.restart_credential()
             print('Background service start normally')
-            time.sleep(10 * 60)
+            time.sleep(1)
 
 
 b_worker = BackgroundWorker()
