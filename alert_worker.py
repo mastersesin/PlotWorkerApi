8b5ec069b5c4f5e15e860b8a78145b0bc881510f
Ty Tran <tytran160995@gmail.com>
2023-04-27 01:44:24 +0700

    Update alert worker

--- alert_worker.py
+++ alert_worker.py
@@ -11,6 +11,7 @@ def test():
     url = f'https://api.telegram.org/bot{telegram_token}/sendMessage'
     print('Alert worker started')
     while True:
+        is_not_empty = False
         # resp = requests.get('http://127.0.0.1:5000/log?sort_type=group')
         resp = requests.get('http://35.238.217.175:5000/log?sort_type=group')
         json_data = resp.json()
@@ -19,10 +20,12 @@ def test():
         for worker in list_worker:
             last_seen = int(worker['last_seen'].split()[0])
             if last_seen > 20:
+                is_not_empty = True
                 text += f'<{worker["ip"]}> has down\n'
         text += f'--End report. Report time: {datetime.datetime.now()}'
         params = {'chat_id': group_id, 'text': text}
-        requests.post(url, params=params)
+        if is_not_empty:
+            requests.post(url, params=params)
         time.sleep(30 * 60)
 
 
